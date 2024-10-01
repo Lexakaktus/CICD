ea111923067ac2dab14dc891b2959c1877c03c19
Ezreal Traci <ezrealtr@un-j1.kzn.21-school.ru>
2024-10-01 12:54:00 +0300

    tests

--- src/cat/tests.sh
+++ src/cat/tests.sh
@@ -200,7 +200,7 @@ echo "SUCCESSFUL: $SUCCESS"
 echo "ALL: $COUNTER"
 printf "\n"
 
-# FAIL=0
+
 if [ $failures != 0 ]
     then
         exit 1
--- src/grep/test_grep.sh
+++ src/grep/test_grep.sh
@@ -113,7 +113,7 @@ done
 
 echo "${GREEN}SUCCESS $SUCCESS ${ENDCOLOR} | ${RED} FAILED $FAIL${ENDCOLOR}";
 
-# FAIL=0
+
 if [ $failures != 0 ]
     then
         exit 1
